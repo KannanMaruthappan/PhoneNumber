fbb60468f07f810c1fb0f2a090175a46ed9646ed
KannanMaruthappan <48598050+KannanMaruthappan@users.noreply.github.com>
2019-03-15 18:45:17 +0000

    Java comments added

--- src/main/java/org/teleprovider/api/phoneno/IPhoneNumberService.java
+++ src/main/java/org/teleprovider/api/phoneno/IPhoneNumberService.java
@@ -3,11 +3,34 @@ package org.teleprovider.api.phoneno;
 import java.io.IOException;
 import java.util.List;
 
+/**
+ * PhoneNumberService interface to retrieve and update details
+ * 
+ * @author KannanMaruthappan
+ */
 public interface IPhoneNumberService {
 
+	/**
+	 * Function used to get all the phone numbers
+	 * 
+	 * @return List<String>
+	 * @throws IOException
+	 */
 	public List<String> getAllNumbers() throws IOException;
 	
+	/**
+	 * Function used to get all the phone numbers for given customer name
+	 * 
+	 * @return List<String>
+	 * @throws IOException
+	 */
 	public List<String> getAllNumbers(String cust) throws IOException;
 	
+	/**
+	 * Function used to activate the phone number for given customer and phone number
+	 * 
+	 * @return boolean
+	 * @throws IOException
+	 */
 	public boolean activatePhoneNumber(String cust, String phoneNo) throws IOException;
 }
